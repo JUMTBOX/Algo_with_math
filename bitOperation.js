cc1d5d3ea6fe0c3371062270ba053432ff26f45f
JUMTBOX <yjey12@naver.com>
2025-10-19 20:48:38 +0900

    ...

--- bitOperation.js
+++ bitOperation.js
@@ -4,4 +4,7 @@
   console.info("BIT OPERATION AND(&) RESULT >>>", (num1 & num2).toString(2)); // 1010
   console.info("BIT OPERATION OR(|) RESULT >>> ", (num1 | num2).toString(2)); // 1111
   console.info("BIT OPERATION XOR(^) RESULT >>>", (num1 ^ num2).toString(2)); // 0101
+
+  console.info(10 << 2 === 40);
+  console.info(40 >> 2 === 10);
 })(11, 14);
